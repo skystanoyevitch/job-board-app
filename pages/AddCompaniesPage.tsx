7d482fced6e90f37df4b1d5b07879e0d91eceffb
Sky <sky.stanoyevitch@gmail.com>
2023-01-29 20:19:38 -0800

    initial setup + dummy pages for testing

---
+++ pages/AddCompaniesPage.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+// export interface IAppProps {}
+
+export default function AddCompaniesComponent() {
+	return (
+		<div>
+			<h1>Add your Compony here!</h1>
+		</div>
+	);
+}
---
+++ pages/AddJobPage.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+// export interface IAppProps {}
+
+export default function AddJobsComponent() {
+	return (
+		<div>
+			<h1>Add a Job for free!</h1>
+		</div>
+	);
+}
