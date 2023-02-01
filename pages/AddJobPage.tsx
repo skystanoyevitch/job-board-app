6756607f14726fd97fc9d52f130479214a1dfca8
skysLaptop <sky.stanoyevitch@gmail.com>
2023-02-01 11:43:14 -0800

    added input fields + drop downs to form

--- pages/AddJobPage.tsx
+++ pages/AddJobPage.tsx
@@ -1,4 +1,6 @@
 import * as React from "react";
+import { Editor } from "react-draft-wysiwyg";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
 // export interface IAppProps {}
 
@@ -9,7 +11,10 @@ export default function AddJobsComponent() {
         <h1 className="text-center text-5xl font-black p-20">
           Add a Job for free
         </h1>
-        <form action="POST" className="container mx-auto w-1/2">
+        <form
+          action="POST"
+          className="container mx-auto w-1/2 flex flex-col space-y-10"
+        >
           <div className="flex justify-center">
             <input
               type="text"
@@ -17,11 +22,92 @@ export default function AddJobsComponent() {
               placeholder="title"
             />
           </div>
-          <div className="mt-10">
+          <div className="">
             <label className="cursor-pointer flex space-x-4">
               <span className="label-text">Remote</span>
               <input type="checkbox" className="checkbox checkbox-success" />
             </label>
+          </div>
+          <div className="">
+            <h2 className="text-xl font-bold">Experience</h2>
+            <div className="mt-6">
+              <label className="cursor-pointer flex space-x-4">
+                <span className="label-text">Entry Level</span>
+                <input type="checkbox" className="checkbox checkbox-success" />
+              </label>
+            </div>
+          </div>
+          <div className="">
+            <h2 className="text-xl font-bold">Employment type</h2>
+            <div className="mt-6">
+              <label className="cursor-pointer flex space-x-4">
+                <span className="label-text">Part Time</span>
+                <input type="checkbox" className="checkbox checkbox-success" />
+              </label>
+            </div>
+          </div>
+          <div className="">
+            <h2 className="text-xl font-bold">Job Description</h2>
+            <Editor
+              editorState={""}
+              onEditorStateChange={""}
+              toolbarClassName=""
+              wrapperClassName="h-96 rounded-md border border-opacity-20"
+              editorClassName=""
+            />
+          </div>
+          <div className="collapse border collapse-arrow">
+            <input type="checkbox" />
+            <div className="collapse-title">
+              <h2 className="text-xl font-bold">Company</h2>
+              <p className="font-lite text-gray-500">
+                name of the hiring company
+              </p>
+            </div>
+            <div className="collapse-content">
+              <input
+                type="text"
+                placeholder="Company name"
+                className="input input-bordered w-full"
+              />
+            </div>
+            <div className="collapse border collapse-arrow">
+              <input type="checkbox" />
+              <div className="collapse-title">
+                <h2 className="text-xl font-bold">How to Apply</h2>
+                <p className="font-lite text-gray-500">application URL</p>
+              </div>
+              <div className="collapse-content">
+                <label className="label">
+                  <span className="label-text font-bold">URL</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="type here"
+                  className="input input-bordered w-full"
+                />
+              </div>
+            </div>
+            <div className="collapse border collapse-arrow">
+              <input type="checkbox" />
+              <div className="collapse-title">
+                <h2 className="text-xl font-bold">Account information</h2>
+                <p className="font-lite text-gray-500">
+                  provide your email address for account creation, your email
+                  and details of account will not show up in the job post.
+                </p>
+              </div>
+              <div className="collapse-content">
+                <label className="label">
+                  <span className="label-text font-bold">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder=""
+                  className="input input-bordered w-full"
+                />
+              </div>
+            </div>
           </div>
         </form>
       </section>
