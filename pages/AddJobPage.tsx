edbf3cc06d1aa2775f3971fc65e72d0bd3005242
skysLaptop <sky.stanoyevitch@gmail.com>
2023-02-02 11:01:51 -0800

    style fixes

--- pages/AddJobPage.tsx
+++ pages/AddJobPage.tsx
@@ -7,8 +7,8 @@ import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 export default function AddJobsComponent() {
   return (
     <>
-      <section className="container mx-auto">
-        <h1 className="text-center text-5xl font-black p-20">
+      <section className="container mx-auto mt-20">
+        <h1 className="text-center text-5xl font-black pb-20">
           Add a Job for free
         </h1>
         <form
