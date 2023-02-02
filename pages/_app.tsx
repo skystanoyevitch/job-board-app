1c8659a3af53cf13d40d7820ebaa4c90e506e9ef
skysLaptop <sky.stanoyevitch@gmail.com>
2023-02-02 11:01:41 -0800

    added nav/footer as components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Navbar } from "@/components/Navbar";
+import { Footer } from "@/components/Footer";
+
+const navLinks = [
+  { id: 0, name: "Home", url: "/" },
+  { id: 1, name: "Companies", url: "/AddCompaniesPage" },
+];
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar navLinks={navLinks} />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
