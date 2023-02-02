1b680cacbc50c97a836ce2a2dd98dfae93545999
skysLaptop <sky.stanoyevitch@gmail.com>
2023-02-02 10:59:15 -0800

    created and initialized footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+export interface IFooterProps {}
+
+export function Footer(props: IFooterProps) {
+  return (
+    <>
+      <section className="bottom-0 h-60 bg-slate-100 mt-20">
+        <div className="container mx-auto">
+          {/* <h1 className="text-center text-2xl">Footer</h1> */}
+        </div>
+      </section>
+    </>
+  );
+}
