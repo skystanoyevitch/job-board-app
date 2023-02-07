3ad2d4b9a3125928297dd9b18d9f54417fd574c9
skysLaptop <sky.stanoyevitch@gmail.com>
2023-02-07 14:46:49 -0800

    minor updates

--- components/JobSearch/JobSearch.tsx
+++ components/JobSearch/JobSearch.tsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import { JobSearchForm } from "./JobSearchForm";
 import { JobSearchResults } from "./JobSearchResults";
 
 export interface IJobSearchProps {}
 
 export function JobSearch(props: IJobSearchProps) {
+  // const [title, setTitle] = useState("");
   return (
     <div>
       <JobSearchForm />
--- pages/AddCompaniesPage.tsx
+++ pages/AddCompaniesPage.tsx
@@ -1,11 +1,25 @@
-import * as React from "react";
+// import clientPromise from "@/database/mongodb.connect";
 
 // export interface IAppProps {}
 
 export default function AddCompaniesComponent() {
-	return (
-		<div>
-			<h1 className="text-3xl text-green-600">Add your Company here!</h1>
-		</div>
-	);
+  return (
+    <div>
+      <h1 className="text-3xl text-green-600">Add your Company here!</h1>
+    </div>
+  );
 }
+
+// export async function getStaticProps() {
+//   try {
+//     const client = await clientPromise;
+//     const db = client.db("job-board-database");
+//     const jobs = await db.collection("jobs").find({}).toArray();
+
+//     return {
+//       props: { jobs: JSON.parse(JSON.stringify(jobs)) },
+//     };
+//   } catch (e) {
+//     console.error(e);
+//   }
+// }
