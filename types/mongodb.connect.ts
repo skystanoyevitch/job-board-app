0a15658e4cc1365c9717feb7088fa4041523d05a
skysLaptop <sky.stanoyevitch@gmail.com>
2023-02-09 07:15:07 -0800

    added type module for jobs

--- types/mongodb.connect.ts
+++ types/mongodb.connect.ts
@@ -1,5 +1,18 @@
 import { MongoClient } from 'mongodb'
 
+// global type variable for persisting values in database
 declare global {
   var _mongoClientPromise: Promise<MongoClient>
 }
+
+//exported job types
+export type createJobType = {
+  title: string;
+  remote: boolean;
+  experience: boolean;
+  employmentType: boolean;
+  companyName: string;
+  applicationUrl: string;
+  userEmail: string;
+};
+
