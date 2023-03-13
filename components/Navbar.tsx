59df79ee43f6a31f5b90877dd8fe8c815ebb2461
skysLaptop <sky.stanoyevitch@gmail.com>
2023-03-13 15:19:53 -0700

    mobile: added menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 export interface NavProps {
   navLinks: {
@@ -12,6 +13,7 @@ export interface NavProps {
 }
 
 export function Navbar(props: NavProps) {
+  const [openMenu, setOpenMenu] = useState(false);
   const router = useRouter();
   const handleClick = (e: any) => {
     // console.log("button clicked!");
@@ -19,8 +21,44 @@ export function Navbar(props: NavProps) {
   };
   return (
     <>
-      <section className="w-full shadow-sm">
-        <nav className="flex justify-between items-center p-4">
+      <section className="w-full shadow-sm z-0">
+        <div className="md:hidden p-4">
+          <button
+            type="button"
+            className="space-y-2"
+            onClick={() => setOpenMenu((prevCheck) => !prevCheck)}
+          >
+            <span className="block w-5 h-0.5 bg-gray-600"></span>
+            <span className="block w-6 h-0.5 bg-gray-600"></span>
+            <span className="block w-8 h-0.5 bg-gray-600"></span>
+          </button>
+        </div>
+
+        {openMenu && (
+          <div className="h-[100vh] z-10">
+            <nav className="text-center flex flex-col">
+              <div className="space-y-8">
+                {props.navLinks.map((link, i) => (
+                  <ul key={i}>
+                    <li>
+                      <Link href={link.url}>{link.name}</Link>
+                    </li>
+                  </ul>
+                ))}
+              </div>
+
+              <button
+                type="button"
+                className="btn btn-outline btn-primary dark:btn-outline dark:btn-accent"
+                onClick={handleClick}
+              >
+                <span className="">Post Job for Free</span>
+              </button>
+            </nav>
+          </div>
+        )}
+
+        <nav className="hidden md:visible md:flex justify-between items-center p-4">
           <div className="flex space-x-8">
             {props.navLinks.map((link, i) => (
               <ul key={i}>
