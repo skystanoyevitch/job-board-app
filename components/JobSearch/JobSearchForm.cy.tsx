af9edaac0ca4ac0af3a010dce8ac2dd989cb4a9f
skysLaptop <sky.stanoyevitch@gmail.com>
2023-03-13 19:58:29 -0700

    comment out code

--- components/JobSearch/JobSearchForm.cy.tsx
+++ components/JobSearch/JobSearchForm.cy.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import { JobSearchForm } from './JobSearchForm'
+// import React from 'react'
+// import { JobSearchForm } from './JobSearchForm'
 
-describe('<JobSearchForm />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<JobSearchForm />)
-  })
-})
+// describe('<JobSearchForm />', () => {
+//   it('renders', () => {
+//     // see: https://on.cypress.io/mounting-react
+//     cy.mount(<JobSearchForm />)
+//   })
+// })
