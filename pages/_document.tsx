378eed988bfb61dbca6b597b767d8fb4a9db063a
skysLaptop <sky.stanoyevitch@gmail.com>
2023-12-07 15:51:45 -0800

    added cupcake theme to html

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en" data-theme="forest">
+    <Html lang="en" data-theme="cupcake">
       <Head />
       <body>
         <Main />
