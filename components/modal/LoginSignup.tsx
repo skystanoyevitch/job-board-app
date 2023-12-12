50d94eb047074f531cf2259eeec5ccab10b228ba
skysLaptop <sky.stanoyevitch@gmail.com>
2023-12-12 12:13:40 -0800

    added buttons to modal

--- components/modal/LoginSignup.tsx
+++ components/modal/LoginSignup.tsx
@@ -5,17 +5,7 @@ export function LoginSignup({ open, close }: any | boolean) {
   if (!open) return null;
   return (
     <>
-      <div className="flex flex-col justify-center shadow-lg top-1/2 left-1/2 fixed -translate-y-1/2 -translate-x-1/2 z-10 bg-white w-full md:max-w-3xl pb-8 md:p-10">
-        <div className="flex justify-end">
-          <button
-            type="button"
-            className="btn btn-xs md:btn-sm btn-circle btn-outline m-2"
-            onClick={close}
-          >
-            x
-          </button>
-        </div>
-
+      <div className="ring-1 ring-slate-900/5 rounded-md flex flex-col justify-center shadow-lg top-1/2 left-1/2 fixed -translate-y-1/2 -translate-x-1/2 z-10 bg-white w-full md:max-w-3xl pb-8 md:p-10">
         <form className="flex flex-col items-center space-y-4 md:space-y-8">
           <div className="label text-xl md:text-3xl font-black bg-clip-text text-transparent bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-fuchsia-500 via-red-600 to-orange-400">
             Create an Account
@@ -29,6 +19,22 @@ export function LoginSignup({ open, close }: any | boolean) {
             placeholder="password"
           ></input>
           <p className="text-xs md:text-sm">Already have an account? Sign in</p>
+
+          <div className="flex space-x-4">
+            <button
+              type="button"
+              className="btn btn-xs md:btn-md md:w-1/2 btn-primary"
+            >
+              done
+            </button>
+            <button
+              type="button"
+              className="btn btn-xs md:btn-md md:w-1/2 btn-error btn-outline"
+              onClick={close}
+            >
+              cancel
+            </button>
+          </div>
         </form>
       </div>
     </>
