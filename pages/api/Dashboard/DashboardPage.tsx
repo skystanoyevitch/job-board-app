cc81416b6a7215558f24e119366fcc4291452893
skysLaptop <sky.stanoyevitch@gmail.com>
2023-12-12 11:42:09 -0800

    Created empty dashboard page

---
+++ pages/api/Dashboard/DashboardPage.tsx
@@ -0,0 +1,13 @@
+type Props = {};
+
+export default function DashboardPage({}: Props) {
+  // TODO: add dashboard UI */
+  // TODO: add profile photo */
+  // TODO: added authentication to access dashboard */
+
+  return (
+    <>
+      <div className="">dashboard</div>
+    </>
+  );
+}
